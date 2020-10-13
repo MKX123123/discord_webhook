5f2c48c04be4ae05fabce0dca0d14d192ea5c003
HazemMeqdad <66125469+HazemMeqdad@users.noreply.github.com>
2020-10-13 21:12:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from discord_webhook import DiscordWebhook
+from discord_webhook import DiscordWebhook, DiscordEmbed
 import os
-data = '''```yor masseg`'''
 
-webhook_urls = ''
 
 from dotenv import load_dotenv
 load_dotenv()
 webhook_urls = os.getenv('webhook')
 
 
-webhook = DiscordWebhook(url=webhook_urls, content=data)
+webhook = DiscordWebhook(url=webhook_urls)
+
+embed = DiscordEmbed(title='Embed Title', description='Your Embed Description', color=242424)
+embed.set_author(name='Author Name', url='https://github.com/HazemMeqdad')
+embed.set_footer(text='Embed Footer Text')
+embed.set_timestamp()
+embed.add_embed_field(name='Field 1', value='Lorem ipsum')
+embed.add_embed_field(name='Field 2', value='dolor sit')
+embed.add_embed_field(name='Field 3', value='amet consetetur')
+embed.add_embed_field(name='Field 4', value='sadipscing elitr')
+
+
+webhook.add_embed(embed)
 
 response = webhook.execute()
